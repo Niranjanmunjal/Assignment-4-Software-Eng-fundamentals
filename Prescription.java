473d9211d7e926c96de6eb94f1de66adfc3bd226
Niranjan Munjal <s4034578@student.rmit.edu.au>
2024-10-23 16:49:10 +1100

    templete

--- Prescription.java
+++ Prescription.java
@@ -1,1 +1,34 @@
+import java.util.ArrayList;
+import java.util.Date;
+public class Prescription{
+    private int prescID;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private float sphere;
+    private float axis;
+    private float cylinder;
+    private Date examinationDate;
+    private String optometrist;
+    private String[] remarktypes= {"Client","Optometrist"};
+    private ArrayList <String> postRemarks= new ArrayList<>();
 
+    public boolean addPrescription()
+    {
+        //TODO Add the prescription's information to a TXT file
+        //If the prescription meets the given conditions,
+        , // the information should be added to a TXT file (e.g., presc.txt), and the 
+        //If the prescription's Information does not meet the given conditions,
+          // the information should not be added to the TXT file, and the function should
+          return True;
+    }
+    public boolean addRemark()
+    {
+        //TODO Add the prescription's remark to a TXT file
+        //IF the remark meets the given Conditions,
+        //the information should be added to a txt File(e.g., remark.txt), and the function should 
+        //If the remark does not meetthe given conditions,
+        //the information should not be added to the TXT file,and the function should 
+        return true;
+    }
+}
